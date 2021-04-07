30b80c9eca1782d573f508c01fa79b7c934b52a6
Elsayed Gamal <eheeba@gmail.com>
2021-04-07 21:01:22 +0200

    finish design

--- scripts/main.js
+++ scripts/main.js
@@ -3,22 +3,26 @@ const cssDiv = document.querySelector('#skills .css');
 const jsDiv = document.querySelector('#skills .js');
 const skillsBar = document.querySelector('#skills .bar');
 
-// Position the HTML skill
-
-htmlDiv.style.right = `${skillsBar.offsetLeft + 100}px`;
-
 function ChangeDiv() {
-  htmlDiv.style.right = `${skillsBar.offsetLeft + 100}px`;
-  cssDiv.style.left = `${skillsBar.offsetLeft + 100}px`;
-  jsDiv.style.right = `${skillsBar.offsetLeft + 100}px`;
+  if (screen.width > 640) {
+    htmlDiv.style.right = `${skillsBar.offsetLeft + 100}px`;
+    cssDiv.style.left = `${skillsBar.offsetLeft + 100}px`;
+    jsDiv.style.right = `${skillsBar.offsetLeft + 100}px`;
+  }
 }
 
 window.addEventListener('resize', ChangeDiv);
 
-// Position the CSS skill
+if (screen.width > 640) {
+  // Position the HTML skill
 
-cssDiv.style.left = `${skillsBar.offsetLeft + 100}px`;
+  htmlDiv.style.right = `${skillsBar.offsetLeft + 100}px`;
 
-// Position the JS skill
+  // Position the CSS skill
 
-jsDiv.style.right = `${skillsBar.offsetLeft + 100}px`;
+  cssDiv.style.left = `${skillsBar.offsetLeft + 100}px`;
+
+  // Position the JS skill
+
+  jsDiv.style.right = `${skillsBar.offsetLeft + 100}px`;
+}
